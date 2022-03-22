ddf628ef81d858f34bfd21ad75dd6a7433cb9cad
pandiarajkumargprku <98823134+pandiarajkumargprku@users.noreply.github.com>
2022-03-22 18:56:16 +0530

    Update DatabaseConnection.java

--- quizapplication/databaseconnection/src/main/java/com/theatmoclub/databaseconncetion/dbconnection/DatabaseConnection.java
+++ quizapplication/databaseconnection/src/main/java/com/theatmoclub/databaseconncetion/dbconnection/DatabaseConnection.java
@@ -8,25 +8,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
+/**
+ * <h1>Database connection</h1>
+ *
+ */
 public class DatabaseConnection {
+
+   private static Map<String,String> property;
 
     public static Connection getConnection() {
 
         try {
-            InputStream fileInputStream = new FileInputStream("etc/system.properties");
-            Properties properties = new Properties();
-
-            properties.load(fileInputStream);
-            String url = properties.getProperty("karaf.jdbc.url");
-            String userName = properties.getProperty("karaf.jdbc.user");
-            String passWord = properties.getProperty("karaf.jdbc.password");
             Class.forName("com.mysql.cj.jdbc.Driver");
-            final Connection connection = DriverManager.getConnection(url, userName, passWord);
+            final Connection connection = DriverManager.getConnection(property.get("url"),property.get("user"), property.get("password"));
 
             return connection;
         } catch (Exception exception) {
             throw new ConnectionFailedException("FAILED TO CONNECTION");
         }
+    }
+
+    public static void database(Map<String, String> properties) {
+        property = properties;
     }
 }
 
